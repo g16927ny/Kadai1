e7c4ad33fea37d041bc5081092822e8cbbc2db94
g16927ny <g16927ny@gm.tsuda.ac.jp>
2018-04-12 14:24:38 +0900

    Third Commit

--- src/Sample.java
+++ src/Sample.java
@@ -2,7 +2,7 @@
 public class Sample {
 
 	public static void main(String[] args){
-		System.out.println("中村です。初めて知ったり使ったりするものが多く、課題も多いということなので不安ではありますが、精一杯取り組みたいと思いました。");
+		System.out.println("中村です。g16927ny/初めて知ったり使ったりするものが多く、課題も多いということなので不安ではありますが、精一杯取り組みたいと思いました。");
 
 	}
 
